c35f6ab8a3dab4266b3066475d15524ac162dea0
castancarl <castancarl@gmail.com>
2022-05-15 20:17:05 +0200

    JS-HEADER
    delete example of OOP and begin work on real JS-menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,24 +1,9 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
 
-import Person from './modules/Person';
+let mobileMenu = new MobileMenu();
 
+/* --------------------------------------------------------------------------- */
 if (module.hot) {
   module.hot.accept();
 }
-
-/* Lesson example code below this line */
-
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + ' now owes zero taxes.');
-  }
-}
-
-let john = new Person('JOHN', 'BLUE'); /* now 'this' = john */
-
-john.greet();
-
-let jane = new Adult('JANE', 'lime');
-
-jane.greet();
-jane.payTaxes();
