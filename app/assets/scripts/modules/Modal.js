a0ba14add7659871b2d25828e3c37f0f664d93d4
castancarl <castancarl@gmail.com>
2022-06-01 14:02:07 +0200

    CODE-SPLIT --> END
    prepare to LAZYLOADING

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -2,19 +2,15 @@ class Modal {
   constructor() {
     this.injectHTML();
     this.modal = document.querySelector('.modal');
-    this.openModalButtons = document.querySelectorAll('.open-modal');
     this.closeIcon = document.querySelector('.modal__close');
     this.events();
   }
 
   events() {
-    //   listen for open click
-    this.openModalButtons.forEach((el) =>
-      el.addEventListener('click', (e) => this.openTheModal(e))
-    );
-    //   listen for close click
+    // listen for close click
     this.closeIcon.addEventListener('click', () => this.closeTheModal());
-    //   pushes any key
+
+    // pushes any key
     document.addEventListener('keyup', (e) => this.keyPressHandler(e));
   }
 
@@ -24,9 +20,7 @@ class Modal {
     }
   }
 
-  openTheModal(e) {
-    // first prevent default behaviour of link clicking;  e = EVENT INFORMATION (default go to some  hyperreference)
-    e.preventDefault();
+  openTheModal() {
     this.modal.classList.add('modal--is-visible');
   }
 
@@ -39,38 +33,22 @@ class Modal {
       'beforeend',
       `
     <div class="modal">
-      <div class="modal__inner">
-        <h2
-          class="section-title section-title--blue section-title--less-margin"
-        >
-          <img src="assets/images/icons/mail.svg" class="section-title__icon" />
-          Get in <strong>Touch</strong>
-        </h2>
-        <div class="wrapper wrapper--narrow">
-          <p class="modal__description">
-            We will have an online order system in place soon. Until then,
-            connect with us on any of the platforms below!
-          </p>
-        </div>
-
-        <div class="social-icons">
-          <a href="#" class="social-icons__icon"
-            ><img src="assets/images/icons/facebook.svg" alt="Facebook"
-          /></a>
-          <a href="#" class="social-icons__icon"
-            ><img src="assets/images/icons/twitter.svg" alt="Twitter"
-          /></a>
-          <a href="#" class="social-icons__icon"
-            ><img src="assets/images/icons/instagram.svg" alt="Instagram"
-          /></a>
-          <a href="#" class="social-icons__icon"
-            ><img src="assets/images/icons/youtube.svg" alt="YouTube"
-          /></a>
-        </div>
+    <div class="modal__inner">
+      <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
+      <div class="wrapper wrapper--narrow">
+        <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
       </div>
-      <div class="modal__close">X</div>
+
+      <div class="social-icons">
+        <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
+        <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
+        <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+        <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
+      </div>
     </div>
-`
+    <div class="modal__close">X</div>
+  </div>
+    `
     );
   }
 }
