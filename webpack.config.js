79d06feeb235ed6e6e9165c82c7a499de50198e9
castancarl <castancarl@gmail.com>
2022-06-02 20:04:09 +0200

    BUILD-PROCESS - CSS extract and minimized
    At this point, we have two scripts DEV and BUILD.
    We also have two copies of the site in separated folders APP and DIST.
    We set WEBPACK.CONFIG.JS to act in different ways (DEV/BUILD) using PROCESS.ENV.NPM_LIFECYCLE_EVENT.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
 const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const postCSSPlugins = [require('postcss-import'), require('postcss-mixins'), require('postcss-simple-vars'), require('postcss-nested'), require('autoprefixer')];
 
 /* ------------------------COMMMON-------------------------------- */
+let cssConfig = {
+  test: /\.css$/i,
+  use: [
+    'css-loader?url=false',
+    {
+      loader: 'postcss-loader',
+      options: { postcssOptions: { plugins: postCSSPlugins } },
+    },
+  ],
+};
+
 let config = {
   entry: './app/assets/scripts/App.js',
   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader?url=false',
-          {
-            loader: 'postcss-loader',
-            options: { postcssOptions: { plugins: postCSSPlugins } },
-          },
-        ],
-      },
-    ],
+    rules: [cssConfig],
   },
 };
 
 /* ------------------------DEV - DEVELOPING----------------------------- */
 if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader');
   config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app'),
@@ -44,6 +46,7 @@ if (currentTask == 'dev') {
 }
 /* ------------------------BUILD - PRODUCTION----------------------------- */
 if (currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
   config.output = {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].js',
@@ -52,10 +55,12 @@ if (currentTask == 'build') {
   config.mode = 'production';
 
   config.optimization = {
-    splitChunks: { chunks: 'all' },
+    splitChunks: { chunks: 'all', minSize: 1000 },
+    minimize: true,
+    minimizer: ['...', new CssMinimizerPlugin()],
   };
 
-  config.plugins = [new CleanWebpackPlugin()];
+  config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' })];
 }
 
 module.exports = config;
