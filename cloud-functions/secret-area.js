1d94e8a44698a1947c2cc8ea067a80c263f4abc5
castancarl <castancarl@gmail.com>
2022-06-05 16:33:57 +0200

    MAIN -- cloud fun test

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,11 @@
 export function handler(event, context, callback) {
+  const secretContent = `
+<h3>Welcome to The Secret Area</h3>
+<p>Here we can tell you that the sky is <strong>blue</strong> an two plus two equals four</p>
+`;
+
   callback(null, {
     statusCode: 200,
-    body: 'Welcome to the super secret area',
+    body: secretContent,
   });
 }
