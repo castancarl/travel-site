58add8f7327f19adb9f3f4d6471e313952571802
castancarl <castancarl@gmail.com>
2022-06-06 13:12:06 +0200

    Update 71. Git Tips - How to Handle (and Avoid) Merge Conflicts.js.js

--- notes/71. Git Tips - How to Handle (and Avoid) Merge Conflicts.js.js
+++ notes/71. Git Tips - How to Handle (and Avoid) Merge Conflicts.js.js
@@ -1,9 +1,18 @@
+// how to avoid a lot of merge conflicts in the first place.
+
+/* TODO: Tip number one is to run the git pull command as often as possible.
+If you're working with a team of other developers, you want to constantly be pulling in their committee from the server so that you're working with the latest code.
+Any time you sit down to work on your project before you actually edit any files, run a get pull.
+*/
+
 /* we will learn how to work with a team of other developers without
 letting merge conflicts and get stress us out. 
  */
 
 // what is emerging conflict.
+/* when John tries to push his master branch to GitHub, he'll see an error message that his push failed because his repo is outdated.( W STOSUNKU DO ZMIENIONEGO PRZEZ JANE's REPO ON GITHUB) */
 
+/* Now, at that point, all John needs to do to fix things is run GIT PULL ORIGIN MASTER to pull in the latest changes from GIT HUB. Then Git can automatically merge his outdated commit with Jane's recent commit.*/
+/* then ESCAPE :wq */
+/*  is able to save the day and merge for us here because the lines of code that each team member added are different from each other. */
 // how to fix or resolve immerge, conflict.
-
-// how to avoid a lot of merge conflicts in the first place.
