f9749f4eaad8701855f14fdaa132b4902781e5f7
castancarl <castancarl@gmail.com>
2022-06-06 14:08:35 +0200

    MAIN - restored STASHED

--- notes/71. Git Tips - How to Handle (and Avoid) Merge Conflicts.js.js
+++ notes/71. Git Tips - How to Handle (and Avoid) Merge Conflicts.js.js
@@ -3,6 +3,13 @@
 /* TODO: Tip number one is to run the git pull command as often as possible.
 If you're working with a team of other developers, you want to constantly be pulling in their committee from the server so that you're working with the latest code.
 Any time you sit down to work on your project before you actually edit any files, run a get pull.
+TODO: Talk with each other about who is responsible for which changes in which parts of the project.
+Be aware of what each team member is working on for that day or week.
+But you'd be surprised, even just having a general idea of what each person is working on
+TODO: get everyone on your team to agree on white space settings in your text editors.
+
+TODO: Tip four won't help you avoid conflicts outright, but if you're working on a feature branch tip for will help you deal with any potential conflicts before it's time to actually merge or feature branch back into master.
+So tip number four is to change the way we think about the git merge command.
 */
 
 /* we will learn how to work with a team of other developers without
